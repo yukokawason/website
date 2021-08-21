34afbf4cf526f3b678915a3802d5f9e185780b05
Jun <noraesae+dev@gmail.com>
2021-08-21 20:39:09 +0900

    Fix header implementation.

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
 
 // Constants
 const MOD_NAV_OPEN = 'nav--open';
+const MOD_NAV_HIGHLIGHT = 'nav--highlight';
+const MOD_NAV_LINKS_OPAQUE = 'nav__links--opaque';
+
+const NAV_TRANSITION_DURATION_MS = 300;
 
 const NAV_MIN_OPACITY_UNTIL = 50;
 const NAV_MAX_OPACITY_FROM = 50;
@@ -12,10 +16,25 @@ const $ = document.querySelector.bind(document);
 const $nav = $('nav');
 const $navContainer = $('.nav__container');
 const $navTitle = $('.nav__title');
+const $navLinks = $('.nav__links');
 
 // Event handlers
+let isClosing = false;
 function toggleNav() {
-  $nav.className = $nav.className ? '' : MOD_NAV_OPEN;
+  const isOpen = $nav.classList.contains(MOD_NAV_OPEN);
+  if (isOpen) {
+    if (isClosing) return;  // Already closing
+    isClosing = true;
+    $nav.classList.remove(MOD_NAV_HIGHLIGHT);
+    $navLinks.classList.remove(MOD_NAV_LINKS_OPAQUE);
+    setTimeout(function () {
+      $nav.classList.remove(MOD_NAV_OPEN);
+      isClosing = false;
+    }, NAV_TRANSITION_DURATION_MS);
+  } else {
+    $nav.classList.add(MOD_NAV_OPEN, MOD_NAV_HIGHLIGHT);
+    $navLinks.classList.add(MOD_NAV_LINKS_OPAQUE);
+  }
 }
 
 function animationFrameStep() {
